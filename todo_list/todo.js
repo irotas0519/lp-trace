90795163f16d97c6b598f0b99fefd93474f27217
iro+ <>
2018-08-29 17:35:20 +0900

    step1のフォームを作成

---
+++ todo_list/todo.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+
+    var todoForm = document.getElementById('todo-form');
+    var todoList = document.getElementById('todo-list');
+
+    var addItem = function(event){
+
+        if(!todoInput.value){
+            return;
+        }
+    }
+})();
